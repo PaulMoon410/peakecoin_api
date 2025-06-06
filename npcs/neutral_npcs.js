fef074948e82cadd2b3f44c41d3e79dee6182392
PaulMoon410 <35177417+PaulMoon410@users.noreply.github.com>
2025-06-06 19:57:46 -0400

    Add files via upload

---
+++ npcs/neutral_npcs.js
@@ -0,0 +1,33 @@
+// neutral_npcs.js - Neutral villagers and townsfolk for Peake RPG
+
+window.neutralNpcs = [
+  {
+    name: "Villager",
+    dialogue: [
+      "Nice weather we're having!",
+      "Have you visited the blacksmith yet?",
+      "I heard the apothecary has new potions."
+    ]
+  },
+  {
+    name: "Child",
+    dialogue: [
+      "Wanna play tag?",
+      "I lost my ball near the well..."
+    ]
+  },
+  {
+    name: "Merchant",
+    dialogue: [
+      "Fresh goods from the city!",
+      "Care to trade?"
+    ]
+  },
+  {
+    name: "Old Man",
+    dialogue: [
+      "Back in my day, the village was smaller.",
+      "The mayor keeps things running smoothly."
+    ]
+  }
+];
